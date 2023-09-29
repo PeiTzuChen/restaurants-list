aa5023a2b8b01707dfc87302837e7849e8a0e314
peitzuchen <prettysna@hotmail.com>
2023-09-29 19:06:46 +0800

    feat: use block parameter to render listening page

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ app.set('view engine', '.hbs');
 app.set('views', './views');
 
 app.use(express.static('./public'))
+const restaurants = require('./public/jsons/restaurant.json').results
 
 app.get('/', (req,res) => {
-res.render('index')
+res.render('index',{restaurants})
 })
 
 app.listen(port, ()=> {
