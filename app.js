11f9a19909ef546585e930dc2416d62a1e23cb05
peitzuchen <prettysna@hotmail.com>
2023-09-29 17:13:53 +0800

    build: use express-hbs as view engine

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const port = 3000
 const app =express()
+const { engine }   = require('express-handlebars');
+
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+app.use(express.static('./public'))
 
 app.get('/', (req,res) => {
-res.send('hi')
+res.render('index')
 })
 
 app.listen(port, ()=> {
