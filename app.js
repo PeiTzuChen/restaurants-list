06330b9fc12b892eca2398e228c3ca75d9c70f9c
peitzuchen <prettysna@hotmail.com>
2023-11-10 16:17:46 +0800

    replace json data with SQL database , revise listening page, detail page & serch function

--- app.js
+++ app.js
@@ -10,28 +10,38 @@ app.set("view engine", ".hbs");
 app.set("views", "./views");
 
 app.use(express.static("public"));
-const restaurants = require("./public/jsons/restaurant.json").results;
 
 app.get("/", (req, res) => {
-  res.render("index", { restaurants });
+  return Restaurant.findAll({
+    raw: true,
+  })
+    .then((restaurants) => res.render("index", { restaurants }))
+    .catch((err) => console.log(err));
 });
 
 app.get("/restaurants/:id", (req, res) => {
   const id = req.params.id;
-  const restaurant = restaurants.find(
-    (element) => element.id.toString() === id
-  );
-  res.render("detail", { restaurant });
+  return Restaurant.findByPk(id, {
+    raw: true,
+  })
+    .then((restaurant) => res.render("detail", { restaurant }))
+    .catch((err) => console.log(err));
 });
 
 app.get("/search", (req, res) => {
   const searchTerm = req.query.keyword.trim().toLowerCase();
-  const restaurantsFiltered = restaurants.filter(
-    (element) =>
-      element.category.toLowerCase().includes(searchTerm) ||
-      element.name.toLowerCase().includes(searchTerm)
-  );
-  res.render("index", { restaurants: restaurantsFiltered, searchTerm });
+  return Restaurant.findAll({
+    raw: true,
+  })
+    .then((restaurants) => {
+      const restaurantsFiltered = restaurants.filter(
+        (restaurant) =>
+          restaurant.category.toLowerCase().includes(searchTerm) ||
+          restaurant.name.toLowerCase().includes(searchTerm)
+      );
+      res.render("index", { restaurants: restaurantsFiltered, searchTerm });
+    })
+    .catch((err) => console.log(err));
 });
 
 app.listen(port, () => {
