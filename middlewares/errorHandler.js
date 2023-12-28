67c76578aa4fcefe2235235dcb7abbbb31ee8067
peitzuchen <prettysna@hotmail.com>
2023-12-28 15:00:55 +0800

    fix bugs

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 module.exports = (error,req,res,next)=> {
   req.flash("error", error.errorMessage || '伺服器錯誤');
   res.redirect("back");
-  next()
+  next(error)
 }
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,5 @@
 'use strict'
-//提示出現2秒就消失
+//提示出現1秒就消失
 const flash = document.querySelector("#flash-message");
 setTimeout(() => {
   flash.remove();
